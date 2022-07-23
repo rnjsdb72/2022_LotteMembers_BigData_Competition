544d1068e1ea61531d4dcea0108ecee8779b529c
KwonYujin <rnjsdb72@gmail.com>
2022-07-23 18:55:59 +0900

    fix import time error & add input() to save file easily

--- modeling/preprocessing.py
+++ modeling/preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 import os
 
 def make_df(prod, serv, cust, prod_info):
@@ -30,12 +31,20 @@ def make_df(prod, serv, cust, prod_info):
         os.makedirs("./data")
     trans.drop_duplicates(inplace = True)
     trans.to_csv("./data/lpay_rec_dataset.txt", header = False, index = False, sep = "\t")
+    print('Save Complete!')
     return trans
 
 if __name__ == "__main__":
-    prod = pd.read_csv(input('상품 구매 정보 데이터 csv 경로를 입력하시오: \t'))
-    serv = pd.read_csv(input('제휴사 이용 정보 csv 경로를 입력하시오: \t'))
-    cust = pd.read_csv(input('고객 데모 정보 데이터 csv 경로를 입력하시오: \t'))
-    prod_info = pd.read_csv(input('상품 분류 정보 데이터 csv 경로를 입력하시오: \t'))
+    yes = input('데이터 경로를 직접 입력을 희망하시면 "yes"를 입력해주세요: \t')
+    if yes == "yes":
+        prod = pd.read_csv(input('상품 구매 정보 데이터 csv 경로를 입력하시오: \t'))
+        serv = pd.read_csv(input('제휴사 이용 정보 csv 경로를 입력하시오: \t'))
+        cust = pd.read_csv(input('고객 데모 정보 데이터 csv 경로를 입력하시오: \t'))
+        prod_info = pd.read_csv(input('상품 분류 정보 데이터 csv 경로를 입력하시오: \t'))
+    else:
+        prod = pd.read_csv('data/LPOINT_BIG_COMP_02_PDDE.csv')
+        serv = pd.read_csv('data/LPOINT_BIG_COMP_03_COP_U.csv')
+        cust = pd.read_csv('data/LPOINT_BIG_COMP_01_DEMO.csv')
+        prod_info = pd.read_csv('data/LPOINT_BIG_COMP_04_PD_CLAC.csv')
 
     df = make_df(prod, serv, cust, prod_info)
