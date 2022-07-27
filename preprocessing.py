f12581658007fb11055aa30984643bf145572be2
KwonYujin <rnjsdb72@gmail.com>
2022-07-27 23:59:59 +0900

    change pd_c cop_c_pd_c when cop_c is starts with A

--- preprocessing.py
+++ preprocessing.py
@@ -7,21 +7,26 @@ from sklearn.preprocessing import RobustScaler
 
 def make_df(prod, serv, cust, prod_info, scaler):
     lst = ['cust', 'rct_no', 'cop_c', 'chnl_dv', 'de_dt', 'de_hr', 'buy_am']
+    prod['pd_c'] = prod[['cop_c', 'pd_c']].apply(lambda x: '_'.join(x), axis=1)
+
     prod['buy_am'] = prod['buy_am'] / prod['buy_ct']
     trans = pd.concat([prod[lst+['pd_c']], serv[lst]])
     trans.pd_c = trans.pd_c.fillna(trans.cop_c)
-    
-    trans = pd.merge(trans, cust, on='cust', how='left')
-    
+
+    trans = pd.merge(trans, cust, on='cust', how='left')    
     trans = pd.merge(trans, prod_info, on='pd_c', how='left')
+
     trans.pd_nm = trans.pd_nm.fillna(trans.cop_c)
     trans.clac_hlv_nm = trans.clac_hlv_nm.fillna(trans.cop_c)
     trans.clac_mcls_nm = trans.clac_mcls_nm.fillna(trans.cop_c)
-    trans = trans.sort_values(by=['cust','de_dt' , 'de_hr']).reset_index(drop = True )
+
     trans['de_dt_month'] = trans['de_dt']//100%100
     trans['de_dt_hr'] = trans['de_dt'].astype('str') + trans['de_hr'].astype('str')
     trans['de_dt_hr'] = pd.to_datetime( trans['de_dt_hr'] , format = '%Y%m%d%H' )
     trans['timestamp'] = trans['de_dt_hr'].apply(lambda x : time.mktime(x.timetuple())).astype("int32")
+
+    trans = trans.sort_values(by=['cust','de_dt' , 'de_hr']).reset_index(drop = True )
+
     lst2 = ['cust','pd_c','timestamp','buy_am','clac_hlv_nm','clac_mcls_nm','pd_nm','chnl_dv','de_dt_month','ma_fem_dv','ages','zon_hlv']
     trans = trans[lst2]
 
@@ -35,8 +40,10 @@ def make_df(prod, serv, cust, prod_info, scaler):
             os.mkdir('./models')
         joblib.dump(transformer, 'models/RobustScaler.pkl')
     
+    ids = []
     for i in lst2[:2] + lst2[4:] : 
         id2 = dict(enumerate(sorted(trans[i].unique())))
+        ids.append({i:id2})
         id2 = {j:i for i, j in id2.items()}
         trans[i] = trans[i].map(lambda x: id2[x]+1)
     
@@ -44,6 +51,7 @@ def make_df(prod, serv, cust, prod_info, scaler):
 
     if not os.path.exists("./data"):
         os.makedirs("./data")
+    joblib.dump(ids, "data/id2cat_cols.pkl")
     trans.drop_duplicates(inplace = True)
     trans.to_csv("./data/lpay_rec_dataset.txt", header = False, index = False, sep = "\t")
     print('Save Complete!')
